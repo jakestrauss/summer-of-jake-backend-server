beedb2eb652ae8c816ee9fec11d3a5004c780642
jakestrauss <jake.strauss217@gmail.com>
2021-11-22 20:01:40 -0800

    Changing back to http port 80, will set up https load balancer in front

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=443 target/server-0.0.1-SNAPSHOT.jar > /home/ec2-user/logs/log.txt 2>&1 &
+sudo java -jar -Dserver.port=80 target/server-0.0.1-SNAPSHOT.jar > /home/ec2-user/logs/log.txt 2>&1 &
